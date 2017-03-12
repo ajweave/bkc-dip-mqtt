718cd89d9a1fbcfbc1f6a6d98ea332a991d6e1a4
Andrew Weaver <andrewjweaver@gmail.com>
2017-03-11 21:18:25 -0600

    Refactoring WIP, net driver sort of working; values not taking

--- test/index.js
+++ test/index.js
@@ -5,7 +5,15 @@ var assert = require('assert');
 describe('bkc-dip-mqtt', function () {
   describe('#format2()', function () {
     it('should format 0 to 00', function() {
-     // assert.equal('00', bkcDipMqtt.format2('0'));
+      assert.equal('00', bkcDipMqtt.format2('0'));
     })
   });
+
+  //Test dbVolumetoBk TODO: Move to test case
+// var p = new PresetParameters('x');
+// for (var i = -80; i < 0; i++) {
+// 	console.log(i.toString(), '=', p.dbVolumeToBk(i));
+// }
+
+
 });
