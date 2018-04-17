d2ccd88bb18e3bb0d7d32bf2bf9c044c0af7c974
Andrew Weaver <andrew@fishlegs.local>
2018-04-16 20:39:24 -0500

    Update tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,25 @@
 //import assert from 'assert';
 var bkcDipMqtt = require('../lib/server.js');
+var p = new PresetParameters('x');
 var assert = require('assert');
 
 describe('bkc-dip-mqtt', function () {
   describe('#format2()', function () {
     it('should format 0 to 00', function() {
-      assert.equal('00', bkcDipMqtt.format2('0'));
+      assert.equal(bkcDipMqtt.format2('0'), '00');
     })
   });
 
-  //Test dbVolumetoBk TODO: Move to test case
-// var p = new PresetParameters('x');
-// for (var i = -80; i < 0; i++) {
-// 	console.log(i.toString(), '=', p.dbVolumeToBk(i));
-// }
+  describe('PresetParameters', function() {
+    describe('#dbVolumeToBk()', function() {
+      it('should convert db to hex', function() {
+        var data = [{db: '-1', bk: '27'}, {db: '-2', bk: '27'}, {db: '-49', bk: 'F'}, {db: '-80', bk: '0'}];
+        data.forEach(d => {
+          assert.equal(p.dbVolumeToBk(d.db, d.bk), d.bk);
+        });
+      })
+    })
+  });
 
 
 });
